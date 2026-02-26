aea9272ba32e192975c83f68c61ba5ca0860829e
Aditya raj Pandey <aditya.119540@stu.upes.ac.in>
2026-02-26 17:08:30 +0530

    Add files via upload

---
+++ PasswordValidatingTool/PasswordMismatchException.java
@@ -0,0 +1,7 @@
+package concepts.PasswordValidatingTool;
+
+public class PasswordMismatchException extends RuntimeException {
+    public PasswordMismatchException(String message) {
+        super(message);
+    }
+}
---
+++ PasswordValidatingTool/PasswordValidator.java
@@ -0,0 +1,44 @@
+package concepts.PasswordValidatingTool;
+
+public class PasswordValidator {
+
+    public void validatePassword(String password , String confirmPassword)throws WeakPasswordException, PasswordMismatchException {
+
+        if (password.length() < 8) {
+            throw new WeakPasswordException("Password must be at least 8 characters long.");
+        }
+
+        if (password.length() > 20) {
+            throw new WeakPasswordException("Password must not exceed 20 characters.");
+        }
+
+        boolean hasUppercase = false;
+        boolean hasLowercase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialCharacter = false;
+        String specialCharacter = "!@#$%^&*_+";
+
+        for(char ch : password.toCharArray()){
+            if(Character.isUpperCase(ch)){
+                hasUppercase = true;
+            }else if(Character.isLowerCase(ch)){
+                hasLowercase = true;
+            }else if(Character.isDigit(ch)){
+                hasDigit = true;
+            }else if(specialCharacter.indexOf(ch) >= 0){
+                hasSpecialCharacter = true;
+            }
+        }
+
+
+        if (!hasUppercase || !hasLowercase || !hasDigit || !hasSpecialCharacter) {
+            throw new WeakPasswordException("Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
+        }
+
+        if (!password.equals(confirmPassword)) {
+            throw new PasswordMismatchException("Passwords do not match.");
+        }
+
+    }
+
+}
